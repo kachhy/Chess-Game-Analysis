f235493aac945f0ff4cb511c9c383a1e20eec415
kachhy <109984629+kachhy@users.noreply.github.com>
2023-07-23 17:51:35 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -41,6 +41,7 @@ args = parse_args()
 # name of the analysis engine
 ENGINE_PATH = args.engine
 
+
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 30, fill = '█', printEnd = "\r"):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
